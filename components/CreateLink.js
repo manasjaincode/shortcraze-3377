90c88472e3840e39b7f24b24ded61c463d38fdaa
manas <manasjaincode@gmail.com>
2024-11-05 23:52:09 +0530

    Updated URLs to point to shortcraze-3377

--- components/CreateLink.js
+++ components/CreateLink.js
@@ -25,7 +25,7 @@ const CreateLink = () => {
       });
       const data = await response.json();
       if (response.ok) {
-        const fullShortenedUrl = `https://Shortcraze.vercel.app/${data.shortenedUrl}`;
+        const fullShortenedUrl = `https://shortcraze-3377.vercel.app/${data.shortenedUrl}`;
         setShortenedUrl(data.shortenedUrl);
         setFullUrl(fullShortenedUrl);
       } else {
