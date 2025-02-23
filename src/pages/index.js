97037ad52bff2442ca5380e2e5464c6e4922e431
manas <manasjaincode@gmail.com>
2025-02-23 14:02:40 +0530

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,12 @@
 import React, { useState } from 'react';
-import SignupForm from './api/SignupForm';
-import LoginForm from '../../components/LoginForm';
+
 import Widget from '../../components/Widget';
 const HomePage = () => {
-  // const [isLogin, setIsLogin] = useState(false);
 
-  // const switchToSignup = () => setIsLogin(false);
-  // const switchToLogin = () => setIsLogin(true);
 
   return (
     <div>
-      {/* {isLogin ? (
-        <LoginForm switchToSignup={switchToSignup} />
-      ) : (
-        <SignupForm switchToLogin={switchToLogin} />
-      )} */}
+    
       <Widget />
     </div>
     
